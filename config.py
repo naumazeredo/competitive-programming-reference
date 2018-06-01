03ab585aee35e5a0ebdeb74386e63d2e7b8b9950
Naum Azeredo <naumazeredo@gmail.com>
2018-06-01 00:48:07 -0300

    Update config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:////tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + basedir + '/test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
     GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from cpref import app
+from cpref import app, db
 
 
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    if app.config['ENV'] == 'development':
+        db.create_all()
+        app.run(use_reloader=True)
+    else:
+        app.run('0.0.0.0', port=4242)
